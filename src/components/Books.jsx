c646fa8b4b2bf61bd1d7494c00546dec0a0492c4
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-15 23:17:17 -0800

    set the api into state and rendered books

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from "react";
+import { fetchBooks } from "../Api";
 
 const Books = () => {
-    return (
-    <>
-    <h2>Books</h2>
-    </>
-    )
+    const [books, setBooks] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+        const {books} = await fetchBooks();
+        setBooks(books);
+    }
+    fetchData();
+  }, [])
+
+  console.log(books);
+  return (
+    <div>
+    <h1>Books</h1>
+    <p> Welcome to our online Library! </p>
+    <p> Take a look at our books: </p>
+    <ul>
+        {books.map(book => (
+          <li className="bookCard" key={book.id}>
+            <h3>{book.title}</h3>
+            <img src={book.coverimage} alt={book.title} />
+            <p>By: {book.author}</p>
+            <p>Description: {book.description}</p>
+          </li>
+        ))}
+    </ul>
+    </div> 
+  )
 }
 export default Books;
