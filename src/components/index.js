9534b9582cbbd6612399405239b23d97d74e51ce
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-15 21:06:54 -0800

    imported and exported components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Account from "./Account";
+import Books from "./Books";
+import Login from "./Login";
+import Register from "./Register";
+import SingleBook from "./SingleBook";
+import Navigations from "./Navigations";
+
+export { Account, Books, Login, Register, SingleBook, Navigations };
