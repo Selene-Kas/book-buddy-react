ea4ee78854902ca8917a599d8fd486d220e3806c
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-15 21:01:11 -0800

    added routes to my components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import Navigations from './components/Navigations'
+import { Books,  Account, Login, Register, SingleBook } from './components'
 import './App.css'
 
 function App() {
-
   return (
     <>
       <div>
-        Hello World 
+        <Navigations />
+        <main>
+          <Routes>
+            <Route path="/" element={<Books />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/singleBook" element={<SingleBook />} />
+          </Routes>
+        </main>
       </div>
     </>
   )
