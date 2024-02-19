da4c3723e672dc5f8ffa5cac13a005ee7ac63d52
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-18 21:10:10 -0800

    added navigation to single book in button

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { fetchBooks } from "../Api";
+import { useNavigate } from "react-router-dom";
 
 const Books = () => {
     const [books, setBooks] = useState([]);
@@ -11,11 +12,12 @@ const Books = () => {
     fetchData();
   }, [])
 
+  const navigate = useNavigate();
+
   console.log(books);
   return (
     <div>
     <h1>Books</h1>
-    <p> Welcome to our online Library! </p>
     <p> Take a look at our books: </p>
     <ul>
         {books.map(book => (
@@ -24,6 +26,7 @@ const Books = () => {
             <img src={book.coverimage} alt={book.title} />
             <p>By: {book.author}</p>
             <p>Description: {book.description}</p>
+            <button onClick={ () => navigate(`books/${book.id}`) }> Book Details </button>
           </li>
         ))}
     </ul>
