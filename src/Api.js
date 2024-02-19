5b59c7874686ca533225a89c673435efd80aa093
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-19 14:00:51 -0800

    added fetch for users/register

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,6 @@
 const API_URL = 'https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api';
 
+// fetch for array of all books
 export async function fetchBooks() {
     try {
   const response = await fetch(`${API_URL}/books`)
@@ -10,8 +11,8 @@ export async function fetchBooks() {
 }
 }
 
+// fetch for a single book
 export const fetchSingleBook = async (bookId) => {
-
     try {
         const response = await fetch(`${API_URL}/books/${bookId}`);
         const result = await response.json();
@@ -22,3 +23,22 @@ export const fetchSingleBook = async (bookId) => {
     }
 };
 //fetchSingleBook(45);
+
+// fetch for registering a user
+export async function fetchRegister(firstname, lastname, email, password) {
+    const response = await fetch(`${API_URL}/users/register`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            firstname,
+            lastname,
+            email,
+            password
+        })
+    });
+    const data = await response.json();
+    return data;
+    //console.log(data);
+}
