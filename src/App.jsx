0ab1f4075526dd9862dd7420fd7385dbe9bae682
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-18 21:07:51 -0800

    added book logo img and state to token

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { Routes, Route } from 'react-router-dom'
 import Navigations from './components/Navigations'
 import { Books,  Account, Login, Register, SingleBook } from './components'
 import './App.css'
+import { useState } from 'react'
+import bookLogo from './assets/books.png'
 
 function App() {
+  const [token, setToken] = useState(null)
   return (
     <>
       <div>
         <Navigations />
         <main>
+        <h1><img id='logo-image' src={bookLogo}/>Library App</h1>
           <Routes>
             <Route path="/" element={<Books />} />
             <Route path="/account" element={<Account />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/singleBook" element={<SingleBook />} />
+            <Route path="/singleBook" element={<SingleBook/>} />
+            <Route path="books/:id" element={<SingleBook />} />
           </Routes>
         </main>
       </div>
