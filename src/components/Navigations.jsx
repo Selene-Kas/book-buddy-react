33fef8ebbddd3f517d6d6f8aab126d336bb65e5c
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-18 21:11:23 -0800

    deleted img from nav bar

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -3,12 +3,6 @@ import { Link } from 'react-router-dom'
 const Navigations = () => {
   return (
    <nav>
-    <header>
-      <div id="logo-image">
-        <Link to="books.png">
-          <img src="books.png" />
-        </Link>
-      </div>
       <ul id="navigations" >
         <li>
           <Link to="/"><h3 className="books"> Books </h3></Link>
@@ -26,7 +20,6 @@ const Navigations = () => {
           <Link to="/register"><h3> Register </h3></Link>
         </li>
       </ul>
-    </header>
   </nav>  
   );
 }
