a6eb92377ff1acbc6a34e2e72535191c55a2d69e
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-29 20:26:42 -0800

    added form and handlelogin function for button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { fetchLogin } from "../Api";
 
-const Login = () => {
+const Login = ( {setToken} ) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+
+    async function handleLogin(event) {
+        event.preventDefault();
+        const data = await fetchLogin(email, password);
+        console.log(data);
+        if (data.token) {
+            localStorage.setItem('token', data.token);
+            setToken(data.token);
+            navigate('/account');
+        }
+    }
+
     return (
     <>
     <h2>Login</h2>
+    <form className='logForm' onSubmit={handleLogin} >
+        <label>
+            <h2>Email:</h2>
+            <input value={email} type="email"
+            onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+            <h2>Password:</h2>
+            <input value={password} type="password"
+            onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button type="submit"> Login </button>
+    </form>
     </>
     )
 }
