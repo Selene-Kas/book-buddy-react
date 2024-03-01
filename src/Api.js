a81c8bbf7376664de67ed6fabcd88a0f81abfe44
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-29 20:16:30 -0800

    got rid of console.logs

--- src/Api.js
+++ src/Api.js
@@ -4,8 +4,8 @@ export const API_URL = 'https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api';
 export async function fetchBooks() {
   try {
   const response = await fetch(`${API_URL}/books`)
-  const result = await response.json();
-  return result;
+  const data = await response.json();
+  return data;
 } catch (error) {
     console.error('Uh oh, trouble fetching books', error);
 }
@@ -16,7 +16,6 @@ export const fetchSingleBook = async (bookId) => {
     try {
         const response = await fetch(`${API_URL}/books/${bookId}`);
         const result = await response.json();
-       console.log(result.book);
         return result.book;
     } catch (err) {
         console.error(`Oh no, trouble fetching book #${bookId}!`, err);
@@ -41,7 +40,6 @@ export async function fetchRegister(firstname, lastname, email, password) {
     });
     const data = await response.json();
     return data;
-    //console.log(data);
     } catch (err) {
         console.error(err);
     } 
@@ -61,10 +59,8 @@ export async function fetchLogin(email, password) {
             })
         });
         const data = await response.json();
-        console.log(data);
         return data;
     } catch (err) {
         console.error(err);
     }
 }
-
