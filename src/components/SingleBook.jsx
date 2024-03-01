3fd12cb7c7f00a9d2e867924ce86a7ca5c0079f4
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-29 20:24:49 -0800

    cleaned code from console.logs

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchSingleBook } from "../Api";
+import { API_URL } from "../Api";
 
 const SingleBook = () => {
     const { id }= useParams();
@@ -11,29 +12,43 @@ const SingleBook = () => {
     async function getBookDetails() {
         const bookDetails = await fetchSingleBook(id);
        setBook(bookDetails)
-       //console.log(bookDetails)
     }
     getBookDetails()
   }, [] )
 
-    return (
-    <div>
-    <h2>Single Book: Id of {id}</h2>
-    {book ? (
-        <ul className="singleBook"> 
-          <li>Title: {book.title}</li>
-          <li><img src={book.coverimage} alt={book.title}  /></li>
-          <li>Author: {book.author}</li>
-          <li>Description: : {book.description}</li>
-          <li>ID: {book.id}</li>
-          <li>Available: {book.available}</li>
-          <button> Checkout Book </button>
-          <button> Return Book </button>
-        </ul>
-        ): (
-          <div>Go back to Books and select Book first.</div>
-        )}
-    </div>
-    )
+  const handleClick = () => {
+    fetch(`${API_URL}/books/` + book.id , {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      },
+      body: JSON.stringify({
+        available: false,
+      })
+    }).then(response => response.json())
+      .then(result => {
+        alert('You successfully checked out the book: '+ book.title);
+      })
+      .catch(console.error);
+  }  
+ 
+  return (
+  <div>
+  <h2>Single Book:</h2>
+  {book ? (
+    <ul className="bookCard"> 
+      <li>Title: {book.title}</li>
+      <li><img src={book.coverimage} alt={book.title}  /></li>
+      <li>Author: {book.author}</li>
+      <li>Description: : {book.description}</li>
+      <li>ID: {book.id}</li>
+      <button onClick={handleClick}> Checkout Book </button>
+    </ul>
+  ): (
+    <h3>Please go back to Books and select Book first to see details.</h3>
+  )}
+  </div>
+  )
 }
 export default SingleBook;
