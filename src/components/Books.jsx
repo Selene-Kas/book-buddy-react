0b76a36395d5d53eb17da27db19256a6642ce809
Kassandra Sanchez <kassandrasmedina@gmail.com>
2024-02-29 20:19:26 -0800

    added search filter to books

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -6,43 +6,34 @@ const Books = () => {
     const [books, setBooks] = useState([]);
     const [error, setError] = useState(null);
     const [filterText, setFilterText] = useState("");
-    //const filteredBooks = books.filter(book => book.name.toLowerCase().includes(filterText.toLowerCase()));
+    const filteredBooks = books.filter( book => 
+      book.title.toLowerCase().includes(filterText.toLowerCase()) ||
+      book.author.toLowerCase().includes(filterText.toLowerCase())
+      );
 
   useEffect(() => {
     async function fetchData() {
-        const {books} = await fetchBooks();
-        //if ({books} = success) {
-        setBooks(books);
-        console.log(books);
-        //} else {
-        //  setError(books.error.message)
-        // }
+        const data = await fetchBooks();
+        setBooks(data.books);
+        console.log(data.books);
     }
     fetchData();
   }, [])
   
   const navigate = useNavigate(); 
 
-  const booksToDisplay = filterText
-    ? books.filter((book) =>
-        book.name.toLowerCase().includes(searchParam.toLowerCase()),
-        console.log(books)
-      )
-    : books;
-
   return (
     <div>
     <p> Take a look at our books: </p>
     <label className="searchInput">
     <input 
-      type="text" placeholder="Search a Book?" 
+      type="text" placeholder="Search a Book?" value={filterText}
       onChange={(e) => setFilterText(e.target.value.toLowerCase())}
     />
     </label>
     {error && <p>{error}</p>}
     <div className="booksList">
-      {booksToDisplay.length > 0
-        ? books.map((book) => {
+      { filteredBooks.map((book) => {
           return(
             <div className="bookCard" key={book.id}>
               <h3>{book.title}</h3>
@@ -52,7 +43,7 @@ const Books = () => {
             </div> 
           );
         })
-        : "No books match search term."} 
+       } 
       </div>
     </div> 
   );
